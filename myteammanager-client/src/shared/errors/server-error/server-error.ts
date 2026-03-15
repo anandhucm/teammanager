ddd486f5244142e480ed52f47e584863422585d4
Anandhu C M <anandhucm@Anandhus-MacBook-Air.local>
2026-03-15 14:21:05 +0530

    team-manager

---
+++ myteammanager-client/src/shared/errors/server-error/server-error.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  imports: [],
+  templateUrl: './server-error.html',
+  styleUrl: './server-error.css',
+})
+export class ServerError {
+
+  private router = inject(Router);
+
+  constructor(){
+    const navigation = this.router.getCurrentNavigation();
+    console.log("navigation", navigation);
+  }
+
+
+
+}
